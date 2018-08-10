7e283aced5fa9ed40407bcde20519be1d0361729
jwwnz <jameswon.nz@gmail.com>
2018-08-10 18:13:14 +1200

    map working

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import SimpleMap from "./components/Map";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <h1 className="App-title">Welcome to Delivery maps</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <SimpleMap />
       </div>
     );
   }
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,35 +1,35 @@
-import React, { Component } from 'react';
-import GoogleMapReact from 'google-map-react';
- 
+import React, { Component } from "react";
+import GoogleMapReact from "google-map-react";
+
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
- 
+
 class SimpleMap extends Component {
   static defaultProps = {
     center: {
       lat: -36.8523379,
       lng: 174.7691077
     },
-    zoom: 11
+    zoom: 14
   };
- 
+
   render() {
     return (
       // Important! Always set the container height explicitly
-      <div style={{ height: '100vh', width: '100%' }}>
+      <div style={{ height: "100vh", width: "100%" }}>
         <GoogleMapReact
-          bootstrapURLKeys={{ key: /* YOUR KEY HERE */ }}
+          bootstrapURLKeys={{ key: "AIzaSyCAA0cjcjXsjZBymMjQuQyZR6hJ0SUf7hI" }}
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
         >
           <AnyReactComponent
             lat={-36.8523379}
             lng={174.7691077}
-            text={'Kreyser Avrora'}
+            text={"Kreyser Avrora"}
           />
         </GoogleMapReact>
       </div>
     );
   }
 }
- 
+
 export default SimpleMap;
